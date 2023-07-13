d72ef939dcaa0aaaed5c877faed0e11659519b5e
root <koining10@gmail.com>
2023-07-13 02:49:09 +0000

    map

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const MyMap = () => {
   // }, []);
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={[23, 120]} zoom={13} style={{ height: "100vh", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
